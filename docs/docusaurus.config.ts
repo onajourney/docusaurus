c8351e3924607af096d692096b282655aad0ec0d
J. Daniels <ayn.pray@gmail.com>
2024-01-09 09:54:19 -0500

    ncie

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -2,21 +2,24 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+const organizationOrUser = 'onajourney';
+const projectName = 'docusaurus';
+
 const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+	url: `https://${organizationOrUser}.example.com`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+	baseUrl: `/${projectName}/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'onajourney', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+	organizationName: `${organizationOrUser}`, // Usually your GitHub org/user name.
+	projectName: `${projectName}`, // Usually your repo name.
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
